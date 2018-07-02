860de936b2058832fc6a7539ba07259c9f91536c
Jennifer Snyder <jlsnyder09@gmail.com>
2018-07-02 13:58:42 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.split("").collect do |letter|
+      alphabet.index(letter)
+    end
+  end
 end
+
+alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+puts alphabetize(expressions)
